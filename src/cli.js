b9ffe6effc4c0022142557c19855d1730de6f650
yana_pfl <yanna.pvl24@gmail.com>
2023-01-16 16:15:35 +0300

    fix src.cli

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const userName = readlineSync.question('May I have your name? ');
+export default function greeting() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
+}
 
-export { userName };
