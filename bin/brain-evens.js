723ef1f18fbac1f79bdba7fbdf7153f057c74750
yana_pfl <yanna.pvl24@gmail.com>
2023-01-17 17:09:18 +0300

    fix brain-evens with index.js

--- bin/brain-evens.js
+++ bin/brain-evens.js
@@ -1,3 +1,13 @@
-import isEven from '../src/even.js';
+import { gameRules, gameCondition } from '../src/index.js';
+import { randomEven, answerEven } from '../src/even.js';
 
-isEven();
+const Rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const questionEven = randomEven();
+const rightAnswerEven = answerEven();
+
+gameRules(Rules);
+
+let winsCounter = 0;
+while (winsCounter < 3) {
+  winsCounter = gameCondition(questionEven, rightAnswerEven);
+}
