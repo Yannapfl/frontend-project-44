fb273f081bfc78a82852ab2a8be458fc6490e718
yana_pfl <yanna.pvl24@gmail.com>
2023-01-20 17:37:45 +0300

    fix calc.js

--- src/calc.js
+++ src/calc.js
@@ -1,8 +1,10 @@
-const randomAction = Math.floor(Math.random() * 3) + 1;
-const a = Math.floor(Math.random() * 100);
+import { gameRules, isGameCondition, runCycleOfWins } from './index.js';
+// create the condition for calculator
+const randomAction = Math.floor(Math.random() * 3) + 1; // random for choosing a sign
+const a = Math.floor(Math.random() * 100); // random for choosing adjectives: a, b
 const b = Math.floor(Math.random() * 100);
 
-const randomCalc = () => {
+const generateCalc = () => { // result of random sign for a mathematical expression
   switch (randomAction) {
     case 1:
       return `${a} + ${b}`;
@@ -12,9 +14,8 @@ const randomCalc = () => {
       return `${a} * ${b}`;
   }
 };
-export { randomCalc };
 
-const answerCalc = () => {
+const countRightAnswerCalc = () => { // result of the mathematical expression
   switch (randomAction) {
     case 1:
       return (a + b);
@@ -25,4 +26,14 @@ const answerCalc = () => {
   }
 };
 
-export { answerCalc };
+// add individual conditions for the Calculator
+const Rules = 'What is the result of the expression?';
+
+// connect variables and functions with index.js
+const isCalc = () => {
+  gameRules(Rules);
+  isGameCondition(generateCalc, countRightAnswerCalc);
+  runCycleOfWins();
+};
+
+export default isCalc;
