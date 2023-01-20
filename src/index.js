b9d82aa1125e15d042ead2c8fb81804f2fe7f886
yana_pfl <yanna.pvl24@gmail.com>
2023-01-20 17:38:25 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -3,28 +3,30 @@ import greeting from './cli.js';
 
 const userName = greeting();
 
-const gameRules = (Rules) => {
+const gameRules = (Rules) => { // description of game's rules
   console.log(Rules);
 };
 
 export { gameRules };
 
 let wins = 0;
-
-const gameCondition = (questionGame, rightAnswer) => {
-  console.log(`Question: ${questionGame}`);
+// comparing the user's answer to the correct one
+const isGameCondition = (isQuestion, isRightAnswer) => {
+  console.log(`Question: ${isQuestion}`);
   const userAnswer = readlineSync.question('Your answer: ');
-  if (`${rightAnswer}` === `${userAnswer}`) {
+  if (`${isRightAnswer}` === `${userAnswer}`) {
     console.log('Correct!');
     wins += 1;
   } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}`);
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isRightAnswer}'.`);
     wins = 4;
   }
   if (wins === 3) {
     console.log(`Congratulations, ${userName}!`);
+  } else {
+    console.log(`Let's try again, ${userName}`);
   }
   return wins;
 };
 
-export { gameCondition };
+export { isGameCondition };
