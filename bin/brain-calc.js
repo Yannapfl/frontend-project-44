7a00f492501e2050b5bbdd9fe1d710363f6af467
yana_pfl <yanna.pvl24@gmail.com>
2023-01-20 17:36:20 +0300

    fix brain-calc with index.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,14 +1,4 @@
-import { gameRules, gameCondition } from '../src/index.js';
-import { randomCalc, answerCalc } from '../src/calc.js';
+#!/usr/bin/env node
+import isCalc from '../src/calc.js';
 
-const Rules = 'What is the result of the expression?';
-const questionCalc = randomCalc();
-const rightAnswerCalc = answerCalc();
-
-// welcome to the brain games
-gameRules(Rules);
-
-let winsCounter = 0;
-while (winsCounter < 3) {
-  winsCounter = gameCondition(questionCalc, rightAnswerCalc);
-}
+isCalc();
