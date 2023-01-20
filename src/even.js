df8e30214a5a668f17192ca4877e1ce22c8d6908
yana_pfl <yanna.pvl24@gmail.com>
2023-01-20 17:38:08 +0300

    fix even.js

--- src/even.js
+++ src/even.js
@@ -1,15 +1,28 @@
-const randomEven = () => Math.floor(Math.random() * 100);
+import { gameRules, isGameCondition, runCycleOfWins } from './index.js';
 
-export { randomEven };
-
-const answerEven = () => {
-  let isEven;
-  if (randomEven % 2 === 0) {
-    isEven = 'yes';
-  } else {
-    isEven = 'no';
-  }
-  return isEven;
+// generate a random number for question
+const generateRandomNumber = () => {
+  const randomNumber = Math.floor(Math.random() * 100);
+  return randomNumber;
 };
 
-export { answerEven };
+const countRightAnswerEven = () => {
+  let rightAnswerEven;
+  if (generateRandomNumber() % 2 === 0) {
+    rightAnswerEven = 'yes';
+  } else {
+    rightAnswerEven = 'no';
+  }
+  return rightAnswerEven;
+};
+
+// add individual conditions for the Calculator
+const Rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+// connect variables and functions with index.js
+const isEven = () => {
+  gameRules(Rules);
+  runCycleOfWins(isGameCondition(generateRandomNumber(), countRightAnswerEven()));
+};
+
+export default isEven;
