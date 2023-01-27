49a577ea338c3364c0fbf3961354ed6d33bb8ddf
yana_pfl <yanna.pvl24@gmail.com>
2023-01-27 16:01:48 +0300

    connect even.js witn index.js

--- src/even.js
+++ src/even.js
@@ -1,14 +1,13 @@
-import { gameRules, isGameCondition, runCycleOfWins } from './index.js';
+import startAlgoritm from './index.js';
 
 // generate a random number for question
-const generateRandomNumber = () => {
-  const randomNumber = Math.floor(Math.random() * 100);
-  return randomNumber;
-};
+const generateRandomNumber = (number) => Math.floor(Math.random() * number);
+export { generateRandomNumber };
 
-const countRightAnswerEven = () => {
+// generate right answer
+const countRightAnswerEven = (randomNumber) => {
   let rightAnswerEven;
-  if (generateRandomNumber() % 2 === 0) {
+  if (randomNumber % 2 === 0) {
     rightAnswerEven = 'yes';
   } else {
     rightAnswerEven = 'no';
@@ -16,13 +15,19 @@ const countRightAnswerEven = () => {
   return rightAnswerEven;
 };
 
+const generateConsidences = () => {
+  const considences = [];
+  considences[0] = generateRandomNumber(100);
+  considences[1] = countRightAnswerEven(considences[0]);
+  return considences;
+};
+
 // add individual conditions for the Calculator
 const Rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 // connect variables and functions with index.js
 const isEven = () => {
-  gameRules(Rules);
-  runCycleOfWins(isGameCondition(generateRandomNumber(), countRightAnswerEven()));
+  startAlgoritm(Rules, generateConsidences);
 };
 
 export default isEven;
