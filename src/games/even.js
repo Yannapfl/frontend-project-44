972f58e16e930e3ddb2ca91bdc2fced313709c9a
yana_pfl <yanna.pvl24@gmail.com>
2023-02-02 17:25:03 +0300

    upgrade even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,8 +3,10 @@ import generateRandomNumber from '../helpers.js';
 
 const isEven = (number) => number % 2 === 0;
 
+const maxNumber = 100;
+
 const generateEven = () => {
-  const questionEven = generateRandomNumber(100); // max number 99
+  const questionEven = generateRandomNumber(maxNumber);
   const answerEven = isEven(questionEven) ? 'yes' : 'no';
   return [questionEven, answerEven];
 };
