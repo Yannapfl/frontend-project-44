49868b341d38f9dba68de2a7214433de133ed163
yana_pfl <yanna.pvl24@gmail.com>
2023-02-02 16:55:43 +0300

    upgrade index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import readlineSync from 'readline-sync';
 import greeting from './cli.js';
 
-// eslint-disable-next-line consistent-return
-const startAlgoritm = (rules, generateRound) => {
+const startAlgoritm = (description, generateRound) => {
   const userName = greeting();
-  console.log(rules);
+  console.log(description);
   const countWins = 3;
   for (let i = 1; i <= countWins; i += 1) {
-    const [questionGame, answerGame] = generateRound(); // add individual considences of games
+    const [questionGame, answerGame] = generateRound();
     console.log(`Question: ${questionGame}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (`${answerGame}` !== `${userAnswer}`) { // response user's and right answers
-      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answerGame}'.\nLet's try again, ${userName}!`);
-    // eslint-disable-next-line no-else-return
-    } else {
-      console.log('Correct!');
-      if (i === countWins) { // three wins in a row
-        return console.log(`Congratulations, ${userName}!`);
-      }
+    if (answerGame !== userAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answerGame}'.\nLet's try again, ${userName}!`);
+      return;
     }
+    console.log('Correct!');
   }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default startAlgoritm;
