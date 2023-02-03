157ae734e11987f13eeddf89fd5b465655fbfbd3
yana_pfl <yanna.pvl24@gmail.com>
2023-02-03 12:51:17 +0300

    fix even and prime

--- src/games/even.js
+++ src/games/even.js
@@ -4,7 +4,7 @@ import generateRandomNumber from '../helpers.js';
 const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
-  const question = generateRandomNumber(0, 100);
+  const question = generateRandomNumber(1, 100);
   const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
